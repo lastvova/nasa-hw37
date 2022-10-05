1b74bbc0fad3297fd3acac7cbbde1cf65afb4702
vlast <lastovskyi.volodymyr@gmail.com>
2022-10-05 21:44:41 +0300

    Initial commit

---
+++ src/test/java/com/example/nasahw37/NasaHw37ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nasahw37;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaHw37ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
